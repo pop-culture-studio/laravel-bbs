104967c0d085f0cc0aa252343e2cf69b758145ae
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-25 11:29:14 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,16 +15,18 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            @auth
+                @include('layouts.navigation')
+            @endauth
 
             <!-- Page Heading -->
-            @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                            {{ config('app.name') }}
+                        </h2>
                     </div>
                 </header>
-            @endif
 
             <!-- Page Content -->
             <main>
