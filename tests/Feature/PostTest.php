2111a8433fee7a73eb077a7e08f31147cb0a8abe
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-25 21:14:59 +0900

    Create PostTest.php

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_store_successful()
+    {
+        $response = $this->post(route('post.store'), [
+            'title' => 'test title',
+            'content' => 'test content',
+            'name' => 'test name',
+            'email' => 'test@localhost',
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect();
+
+        $this->assertDatabaseCount('posts', 1)
+             ->assertDatabaseHas('posts', [
+                 'title' => 'test title',
+                 'content' => 'test content',
+                 'name' => 'test name',
+                 'email' => 'test@localhost',
+             ]);
+    }
+
+    public function test_store_invalid()
+    {
+        $response = $this->post(route('post.store'), [
+            'title' => 'test title',
+            'content' => '',
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect()
+                 ->assertInvalid(['content']);
+
+        $this->assertDatabaseCount('posts', 0)
+             ->assertDatabaseMissing('posts', [
+                 'title' => 'test title',
+             ]);
+    }
+}
