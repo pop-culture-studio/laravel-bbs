9a0171db71019f55d61a95eb8b968c8855f3e592
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-26 17:05:14 +0900

    Update CommentFactory.php

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -17,7 +17,10 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'content' => fake()->text(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
     }
 }
