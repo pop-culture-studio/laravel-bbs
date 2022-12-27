6baa0e57b6e37f4cb2b70642aec81fd55a089205
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-27 12:14:44 +0900

    Update form.blade.php

--- resources/views/home/form.blade.php
+++ resources/views/home/form.blade.php
@@ -41,6 +41,7 @@
         <x-text-input id="password" class="block mt-1 w-full"
                       type="password"
                       name="password"
+                      required
                       autocomplete="password"/>
 
         <x-input-error :messages="$errors->get('password')" class="mt-2"/>
