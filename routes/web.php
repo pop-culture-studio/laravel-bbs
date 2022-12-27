d7a43a9b444a95301cb51c15b5ce34ee005553bf
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-27 13:49:49 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', HomeController::class)->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard')->can('admin');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
