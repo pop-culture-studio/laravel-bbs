1c30986af00d1683425770d29b653deed343720b
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-28 13:06:58 +0900

    icon

---
+++ config/icon.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'icon1' => [
+        'name' => 'アイコン 1',
+        'file' => 'icon1.jpg',
+    ],
+    'icon2' => [
+        'name' => 'アイコン 2',
+        'file' => 'icon2.jpg',
+    ],
+    'icon3' => [
+        'name' => 'アイコン 3',
+        'file' => 'icon3.jpg',
+    ],
+    'icon4' => [
+        'name' => 'アイコン 4',
+        'file' => 'icon4.jpg',
+    ],
+    'icon5' => [
+        'name' => 'アイコン 5',
+        'file' => 'icon5.jpg',
+    ],
+];
