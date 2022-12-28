62c7e8683a7f559fe6fa1748ea907afd7e873686
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-28 11:25:25 +0900

    Create PostDeleteConfirmController.php

---
+++ app/Http/Controllers/PostDeleteConfirmController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostDeleteConfirmController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  Request  $request
+     * @param  Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request, Post $post)
+    {
+        return view('home.post-delete-confirm')->with(compact('post'));
+    }
+}
