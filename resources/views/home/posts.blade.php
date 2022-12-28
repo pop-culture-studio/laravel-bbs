f32a54090ff19c04299be5e11a2bba2feb2b9c91
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-28 09:47:06 +0900

    Update posts.blade.php

--- resources/views/home/posts.blade.php
+++ resources/views/home/posts.blade.php
@@ -1,14 +1,25 @@
 @forelse($posts as $post)
-    <div class="flex flex-row">
-        <div class="p-6 my-3 w-1/4">
+    <div class="flex flex-col sm:flex-row">
+        <div class="px-6 sm:p-6 my-3 sm:w-1/4">
             <div class="text-lg font-bold">{{ $post->name ?? 'NO NAME' }}</div>
 
             <time class="mt-6" datetime="{{ $post->created_at }}">{{ $post->created_at }}</time>
         </div>
-        <div class="grow my-3 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="grow sm:my-3 bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-lg text-gray-900">
-                <h2 class="font-bold inline">{{ $post->title ?? 'NO TITLE' }}</h2><span
-                    class="text-gray-300 font-medium ml-3">#{{ $post->id }}</span>
+                <div class="flex space-x-3">
+                    <h2 class="font-bold">
+                        {{ $post->title ?? 'NO TITLE' }}
+                    </h2>
+
+                    <div class="text-gray-400 font-medium">
+                        #{{ $post->id }}
+                    </div>
+
+                    <div>
+                        @include('home.post-menu')
+                    </div>
+                </div>
                 <p class="py-2">
                     {!! nl2br(e($post->content)) !!}
                 </p>
