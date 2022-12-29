029066bc0ba6676a81772cec8a805f286d1b7a09
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-29 14:28:59 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,4 +14,14 @@
             @include('home.posts')
         </div>
     </div>
+
+    <div class="py-12">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    @include('home.visitors')
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
